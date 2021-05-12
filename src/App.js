e52c4d39c4ff161446a3f13abb55521e16912c94
Oleksandr Ivashchuk <thelookout84@gmail.com>
2021-05-12 17:16:32 +0300

    update private and public routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,82 @@
-import React, { Component } from 'react';
-import PhoneBook from '../src/Views/PhoneBook';
+import React, { Component, Suspense, lazy } from 'react';
+// import PhoneBook from '../src/Views/PhoneBook';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Container from './components/Container';
 import { authOperations } from './redux/Auth';
-import AppBar from './components/AppBar';
-import LoginPage from './Views/LoginPage';
-import RegisterPage from './Views/RegisterPage';
-import HomePage from './Views/HomePage';
+import AppBar from './components/AppBar/AppBar';
+// import LoginPage from './Views/LoginPage';
+// import RegisterPage from './Views/RegisterPage';
+// import HomePage from './Views/HomePage';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+
+const HomePage = lazy(() =>
+  import('./Views/HomePage' /* webpackChunkName: "home-page" */),
+);
+
+const PhoneBookPage = lazy(() =>
+  import('./Views/PhoneBook' /* webpackChunkName: "phonebook-page" */),
+);
+
+const RegisterPage = lazy(() =>
+  import('./Views/RegisterPage' /* webpackChunkName: "register-page" */),
+);
+
+const LoginPage = lazy(() =>
+  import('./Views/LoginPage' /* webpackChunkName: "login-page" */),
+);
 
 class App extends Component {
   componentDidMount() {
     this.props.onGetCurretnUser();
   }
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <PhoneBook />
-//       </div>
-//     );
-//   }
-// }
+  //   render() {
+  //     return (
+  //       <div className="App">
+  //         <PhoneBook />
+  //       </div>
+  //     );
+  //   }
+  // }
 
-// export default App;
+  // export default App;
 
-
-
-
-render() {
-  return (
-    <Container>
-      <AppBar />
-
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/contacts" component={PhoneBook} />
-      </Switch>
-    </Container>
-  );
-}
+  render() {
+    return (
+      <Container>
+        <AppBar />
+        <Suspense fallback="">
+          <Switch>
+            <PublicRoute exact path="/" component={HomePage} />
+            <PrivateRoute
+              path="/contacts"
+              redirectTo="/login"
+              component={PhoneBookPage}
+            />
+            <PublicRoute
+              path="/register"
+              restricted
+              redirectTo="/contacts"
+              component={RegisterPage}
+            />
+            <PublicRoute
+              path="/login"
+              restricted
+              redirectTo="/contacts"
+              component={LoginPage}
+            />
+          </Switch>
+        </Suspense>
+      </Container>
+    );
+  }
 }
 
 const mapDispatchToProps = {
-onGetCurretnUser: authOperations.getCurrentUser,
+  onGetCurretnUser: authOperations.getCurrentUser,
 };
 
 export default connect(null, mapDispatchToProps)(App);
